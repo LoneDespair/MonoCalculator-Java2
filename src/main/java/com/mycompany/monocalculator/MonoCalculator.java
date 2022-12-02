b61216b285de1f3ea16b790465d8c1166f38107a
LoneDespairYt <lonedespairyt@gmail.com>
2022-12-02 14:22:15 +0800

    added backspace

---
+++ src/main/java/com/mycompany/monocalculator/MonoCalculator.java
@@ -0,0 +1,11 @@
+package com.mycompany.monocalculator;
+
+
+public class MonoCalculator {
+
+    public static void main(String[] args) {
+        new Classic().setVisible(true);
+        
+        System.out.println("Hello World!");
+    }
+}
